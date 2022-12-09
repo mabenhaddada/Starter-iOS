c49f5fc27072da548e4ea2ca6184abc3f91337d4
Mohamed Amine BEN HADDADA <ben.haddada.mohamed.amine@gmail.com>
2022-12-09 10:33:25 +0100

    Untrack Pods

--- Pods/Target Support Files/Pods-StarterTests/Pods-StarterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StarterTests : NSObject
-@end
-@implementation PodsDummy_Pods_StarterTests
-@end
--- Pods/Target Support Files/RxGRDB/RxGRDB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxGRDB : NSObject
-@end
-@implementation PodsDummy_RxGRDB
-@end
